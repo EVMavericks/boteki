d47cdb44a86f17bcb41a7c86a07bae3021db66d5
Roberto Henríquez Perozo <rihp94@gmail.com>
2021-02-05 01:03:19 +0100

    starting to work with basic tweet objects

--- README.md
+++ README.md
@@ -8,11 +8,31 @@ Set up variables on the config.py file
 
 ## Process:
 
-1. Connect Discord bot through official API.
-1. Connect to Twitter through Tweepy
+- [x]. Connect Discord bot through official API.
+- [x] Connect to Twitter through Tweepy
 
-1. Query specific Discord #Twitter channel message history .
-- User handle
+- [x] Receinve `!tweet` specific commands on Discord #Twitter channel.
+- [x] Record votes after X time
+  
+Next step:
+
+Log entries
+
+```
+ tweet: {
+     'author': 
+        builder: {
+            'discord_user_id':'',
+            'eth_address':'',
+         },
+     'content':'',
+     'discord_message_id':'',
+     'score': ,
+     'result': ['pending', 'passed', 'rejected']
+ }
+```
+
+- [ ] User handle
 - Tweet text (Emoji compliant)
 - Message permalink / _id
 - Confirm tweet length.
--- mongo.py
+++ mongo.py
@@ -1,7 +1,42 @@
+# %% 
 from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
 
-client = MongoClient('mongodb://localhost:27017')
+ATLAS = os.getenv('ATLAS')
+MONGO_DB_NAME = os.getenv('MONGO_DB_NAME')
 
-db = client.test_database
+def connect():
+    print("connecting")
+    client = MongoClient(f"mongodb+srv://rihp:{ATLAS}@messenger-api.lq6n5.mongodb.net/{MONGO_DB_NAME}?retryWrites=true&w=majority")
+    db = client.test
+    print("Connected")
+    return db
 
-collection = db.test_collection
+db = connect()
+
+#%%
+
+tweet =  {
+     'author':'discord_user_id',
+     'content':'',
+     'discord_message_id':'',
+     'score': 0,
+     'result': ['pending', 'passed', 'rejected']
+ }
+
+def submit_tweet(tweet):
+    db.tw
+    post_id = db.tweets.insert_one(tweet).inserted_id
+    print(post_id)    
+    return post_id
+
+submit_tweet(tweet)
+# %%
+db.list_collection_names()
+
+# %%
+
+db.tweets.find_one()
+
+# %%
