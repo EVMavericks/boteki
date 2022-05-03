fd07988b8658b8033d9e44f84ef94a9afbce8292
Nick <20791144+4E-69-63-6B-20-42-6F-73@users.noreply.github.com>
2022-05-03 16:36:54 +0200

    use connectionstring

--- mongo.py
+++ mongo.py
@@ -5,12 +5,10 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-ATLAS = os.getenv('ATLAS')
-MONGO_DB_NAME = os.getenv('MONGO_DB_NAME')
-
+ATLAS_CONNECTIONSTRING = os.getenv('ATLAS_CONNECTIONSTRING')
 def connect():
     print("connecting")
-    client = MongoClient(f"mongodb+srv://rihp:{ATLAS}@messenger-api.lq6n5.mongodb.net/{MONGO_DB_NAME}?retryWrites=true&w=majority")
+    client = MongoClient(ATLAS_CONNECTIONSTRING)
     db = client.test
     print("Connected")
     return db
