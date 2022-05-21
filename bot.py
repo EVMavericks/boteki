50c5691ff8b7be1ba7bdc42bc3ce762eac59513c
Nick <20791144+4E-69-63-6B-20-42-6F-73@users.noreply.github.com>
2022-05-21 09:29:53 +0200

    Added try-except

--- bot.py
+++ bot.py
@@ -99,28 +99,31 @@ async def validateTweets(ctx):
     for tweet in mongo.db.tweets.find({'status':'pending', 'guild': guild}):
         
         print(f"{tweet=}")
-        newstatus = await bot.get_channel(tweet['channel']).fetch_message(int(tweet['poll']))            
-        results = democratic.count_votes(f"{newstatus.reactions}")
-        net_score = democratic.net_score(results)
+        try:
+            newstatus = await bot.get_channel(tweet['channel']).fetch_message(int(tweet['poll']))            
+            results = democratic.count_votes(f"{newstatus.reactions}")
+            net_score = democratic.net_score(results)
 
-        if net_score >= config.required_score: 
-            
-            # This sends the tweet through tweepy.
-            try:
-                await tweetBot.send(f"This tweet has a `net_score` of {net_score}, which is compliant with the minimum publish threshold of {config.required_score} points in favor.")
-                await tweetBot.send(f"```\n{tweet['content']}\n```")
-                response = tweetClient.tweet_send(tweet['content'], guild)._json
-           
-                # TODO: change mongo document to include the new tweet's URL and also include the new net_score 
-                print(f"{response['id']=}")
+            if net_score >= config.required_score: 
                 
-                mongo.confirm_tweet(tweet['_id'], response)
-                await tweetBot.send(f"Tweet above has been sent (https://twitter.com/twitter/statuses/{response['id']}) and database object updated.")
-            except Exception as e:
-                await tweetBot.send(f"error occurred when sending tweet:\n{e}")
+                # This sends the tweet through tweepy.
+                try:
+                    await tweetBot.send(f"This tweet has a `net_score` of {net_score}, which is compliant with the minimum publish threshold of {config.required_score} points in favor.")
+                    await tweetBot.send(f"```\n{tweet['content']}\n```")
+                    response = tweetClient.tweet_send(tweet['content'], guild)._json
+            
+                    # TODO: change mongo document to include the new tweet's URL and also include the new net_score 
+                    print(f"{response['id']=}")
+                    
+                    mongo.confirm_tweet(tweet['_id'], response)
+                    await tweetBot.send(f"Tweet above has been sent (https://twitter.com/twitter/statuses/{response['id']}) and database object updated.")
+                except Exception as e:
+                    await tweetBot.send(f"error occurred when sending tweet:\n{e}")
 
-        else: print(f" ~ Tweet skipped with a {net_score=}")
-
+            else:
+                print(f" ~ Tweet skipped with a {net_score=}")
+        except Exception as e:
+            print(e)
     await tweetBot.send(f"done validating :)")
-        
+
 bot.run(TOKEN)
